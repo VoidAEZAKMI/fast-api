2aab7514a5779c7365d4f97e5fbb5166130afb9f
Reversep <adilet.t.k@gmail.com>
2025-06-30 21:32:02 +0600

    added migration with alembic

---
+++ src/migrations/versions/f3d837c631b3_initial_migration.py
@@ -0,0 +1,35 @@
+"""initial migration
+
+Revision ID: f3d837c631b3
+Revises: 
+Create Date: 2025-06-30 21:22:17.679712
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f3d837c631b3'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table('hotels',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('title', sa.String(length=100), nullable=False),
+    sa.Column('location', sa.String(), nullable=False),
+    sa.PrimaryKeyConstraint('id')
+    )
+
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table('hotels')
+
