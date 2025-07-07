73ada9e2b30053b3f1bb2dbc9c30372b8568675c
Reversep <adilet.t.k@gmail.com>
2025-07-08 00:02:20 +0600

    add filtration for get query

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 class ParamsPagination(BaseModel):
     page:  Annotated[int | None,  Query(1,  ge=1, description="Номер страницы (по умолчанию 1)")]
-    per_page: Annotated[int | None,  Query(3,  ge=1, description="Записей на странице (по умолчанию 3)")]
+    per_page: Annotated[int | None,  Query(None,  ge=1, description="Записей на странице (по умолчанию 3)")]
 
 
 PaginationDep = Annotated[ParamsPagination, Depends()]
--- src/api/hotels.py
+++ src/api/hotels.py
@@ -16,19 +16,24 @@ async def get_hotels(
     title: str | None = Query(None, description="Название отеля"),
     
 ):
+    per_page = pagination.per_page or 5
     async with async_session_maker() as session:
         query = select(HotelsOrm)
+        if id:
+            query = query.filter_by(id=id)
+        if title:
+            query = query.filter_by(title=title)
+        query = (
+            query
+            .limit(per_page)
+            .offset(per_page * (pagination.page - 1))
+        )
         result = await session.execute(query)
 
         hotels = result.scalars().all()
         print(type(hotels), hotels)
         return hotels
 
-
-
-     
-    #  if pagination.page and pagination.per_page:
-    #       return hotels_[pagination.per_page * (pagination.page-1):][:pagination.per_page]
 
 
 @router.post("")
